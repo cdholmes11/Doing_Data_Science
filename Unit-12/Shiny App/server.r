3843494f4828c1f9640ad0cf2c9655444447c176
cdholmes11 <coreydaneholmes@gmail.com>
2022-01-19 17:57:11 -0600

    Shiny App Update

--- Unit-12/Shiny App/server.r
+++ Unit-12/Shiny App/server.r
@@ -2,19 +2,25 @@
 library(magrittr)
 library(tidyverse)
 library(ggplot2)
+library(rsconnect)
 
-beers_raw <- read.csv("C:/Users/corey/OneDrive/Documents/GitHub/DDS-Project-1/Beers.csv", header = TRUE, encoding = "UTF-8")
-breweries_raw <- read.csv("C:/Users/corey/OneDrive/Documents/GitHub/DDS-Project-1/Breweries.csv", header = TRUE, encoding = "UTF-8")
+
+beers_raw <- read.csv("Beers.csv", header = TRUE, encoding = "UTF-8")
+breweries_raw <- read.csv("Breweries.csv", header = TRUE, encoding = "UTF-8")
 
 brew_beers <-
   left_join(beers_raw, breweries_raw, by = c("Brewery_id" = "Brew_ID")) %>%
-  select(Name.x, Beer_ID, ABV, IBU, Brewery_id, Style, Ounces, Name.y, City,
-         State) %>%
+  select(
+    Name.x, Beer_ID, ABV, IBU, Brewery_id, Style, Ounces, Name.y, City,
+    State
+  ) %>%
   filter(!is.na(ABV) | !is.na(IBU)) %>%
   rename("beer_name" = "Name.x") %>%
-  rename("brewery_name" =  "Name.y")
+  rename("brewery_name" = "Name.y")
 
-states <- brew_beers %>% select(State) %>% distinct()
+states <- brew_beers %>%
+  select(State) %>%
+  distinct()
 states <- states[order(states$State), ]
 
 
@@ -22,67 +28,82 @@ server <- function(input, output) {
   # Scatter Plot
   output$dist_plot3 <- renderPlot({
     if (input$select == "on") {
-      if(input$select2 == "All") {
+      if (input$select2 == "All") {
         brew_beers %>%
           ggplot(aes(x = IBU, y = ABV)) +
           geom_point() +
           geom_smooth(method = "lm") +
           theme_minimal()
-      }
-      else {
+      } else {
         brew_beers %>%
           filter(State == input$select2) %>%
           ggplot(aes(x = IBU, y = ABV)) +
           geom_point() +
           geom_smooth(method = "lm") +
-          theme_minimal()        
+          theme_minimal()
       }
-      
+    } else {
+      if (input$select2 == "All") {
+        brew_beers %>%
+          ggplot(aes(x = IBU, y = ABV)) +
+          geom_point() +
+          theme_minimal()
+      } else {
+        brew_beers %>%
+          filter(State == input$select2) %>%
+          ggplot(aes(x = IBU, y = ABV)) +
+          geom_point() +
+          theme_minimal()
+      }
     }
-    else {
-      brew_beers %>%
-        filter(State == input$select2) %>%
-        ggplot(aes(x = IBU, y = ABV)) +
-        geom_point() +
-        theme_minimal()
-    }
-    
   })
-  
+
   # ABV
   output$dist_plot <- renderPlot({
-    
     dist <- switch(input$dist,
-                   hist = geom_histogram(fill = "blue", color = "white"),
-                   box = geom_boxplot(fill = "blue"),
+      hist = geom_histogram(fill = "blue", color = "white"),
+      box = geom_boxplot(fill = "blue"),
     )
-    
-    brew_beers %>%
-      filter(State == input$select2) %>%
-      ggplot(aes(x = ABV)) +
-      dist +
-      ylab("ABV") +
-      theme_minimal()
+    if (input$select2 == "All") {
+      brew_beers %>%
+        ggplot(aes(x = ABV)) +
+        dist +
+        ylab("ABV") +
+        theme_minimal()
+    } else {
+      brew_beers %>%
+        filter(State == input$select2) %>%
+        ggplot(aes(x = ABV)) +
+        dist +
+        ylab("ABV") +
+        theme_minimal()
+    }
   })
-  
+
   # IBU
   output$dist_plot2 <- renderPlot({
-    
     dist <- switch(input$dist,
-                   hist = geom_histogram(fill = "blue", color = "white"),
-                   box = geom_boxplot(fill = "blue"),
+      hist = geom_histogram(fill = "blue", color = "white"),
+      box = geom_boxplot(fill = "blue"),
     )
-    
-    brew_beers %>%
-      filter(State == input$select2) %>%
-      ggplot(aes(x = IBU)) +
-      dist +
-      ylab("IBU") +
-      theme_minimal()
+    if (input$select2 == "All") {
+      brew_beers %>%
+        ggplot(aes(x = IBU)) +
+        dist +
+        ylab("IBU") +
+        theme_minimal()
+    } else {
+      brew_beers %>%
+        filter(State == input$select2) %>%
+        ggplot(aes(x = IBU)) +
+        dist +
+        ylab("IBU") +
+        theme_minimal()
+    }
   })
-  
+
   # Median ABV by State
-  
+
   output$dist_plot4 <- renderPlot({
     median_state_abv <-
       brew_beers %>%
@@ -91,15 +112,16 @@ server <- function(input, output) {
       summarize(median_abv = median(ABV)) %>%
       as.data.frame()
     median_state_abv
-    
+
     median_state_abv %>%
       ggplot(aes(
         x = reorder(State, -median_abv),
-        y = median_abv * 100)
-      ) +
+        y = median_abv * 100
+      )) +
       geom_bar(stat = "identity", fill = "#2a5769") +
       geom_text(aes(label = median_abv * 100),
-                stat = "identity", vjust = -1, size = 3) +
+        stat = "identity", vjust = -1, size = 3
+      ) +
       xlab("State") +
       ylab("Median ABV") +
       ggtitle("Median ABV by State") +
--- Unit-12/Shiny App/ui.r
+++ Unit-12/Shiny App/ui.r
@@ -1,62 +1,78 @@
+# Libraries
 library(shiny)
 library(magrittr)
 library(tidyverse)
 library(rsconnect)
 
 # Data inport
-beers_raw <- read.csv("C:/Users/corey/OneDrive/Documents/GitHub/DDS-Project-1/Beers.csv", header = TRUE, encoding = "UTF-8")
-breweries_raw <- read.csv("C:/Users/corey/OneDrive/Documents/GitHub/DDS-Project-1/Breweries.csv", header = TRUE, encoding = "UTF-8")
+beers_raw <- read.csv("Beers.csv", header = TRUE, encoding = "UTF-8")
+breweries_raw <- read.csv("Breweries.csv", header = TRUE, encoding = "UTF-8")
 
+# Data Merge
 brew_beers <-
   left_join(beers_raw, breweries_raw, by = c("Brewery_id" = "Brew_ID")) %>%
-  select(Name.x, Beer_ID, ABV, IBU, Brewery_id, Style, Ounces, Name.y, City,
-         State) %>%
+  select(
+    Name.x, Beer_ID, ABV, IBU, Brewery_id, Style, Ounces, Name.y, City,
+    State
+  ) %>%
   filter(!is.na(ABV) | !is.na(IBU)) %>%
   rename("beer_name" = "Name.x") %>%
-  rename("brewery_name" =  "Name.y")
+  rename("brewery_name" = "Name.y")
 
-states <- brew_beers %>% select(State) %>% distinct()
+# States Dataframe
+states <- brew_beers %>%
+  select(State) %>%
+  distinct()
 states <- states[order(states$State), ]
 
 ui <- fluidPage(
   # Title
-  titlePanel("Beer Analysis"),
-  
-  # Side panel for selectors
-  sidebarLayout(
-    
-    column(3, sidebarPanel(
-      radioButtons("dist", "Chart Type",
-                   c("Histogram" = "hist",
-                     "Boxplot" = "box")
-      ),
-      
-      selectInput("select",
-                  label = h3("Linear Regression Toggle"),
-                  choices = c("On" = "on", "Off" = "off"),
-                  selected = 1
-      ),
-      
-      selectInput("select2",
-                  label = h3("State Selection"),
-                  choices = c("All", states),
-                  selected = 1
-      ),
-      hr(),
-      fluidRow(column(3, verbatimTextOutput("value")))
-    ),
-    
-    # Main panel for displaying output
-    mainPanel("ABV and IBU Charts",
-              fluidRow(
-                column(6, plotOutput(outputId = "dist_plot")),
-                column(6, plotOutput(outputId = "dist_plot2")),
-                column(6, plotOutput(outputId = "dist_plot3")),
-                column(6, plotOutput(outputId = "dist_plot4"))
-              )
+  fluidRow(
+    column(12, 
+           tags$h1("Beer Analysis")
     )
+  ),
+  
+  # Side Panel for Inputs
+  fluidRow(
+    column(3,
+           radioButtons(
+             "dist", "Chart Type",
+             c(
+               "Histogram" = "hist",
+               "Boxplot" = "box"
+             )
+           ),
+           selectInput("select",
+                       label = h3("Linear Regression Toggle"),
+                       choices = c("On" = "on", "Off" = "off"),
+                       selected = 1
+           ),
+           selectInput("select2",
+                       label = h3("State Selection"),
+                       choices = c("All", states),
+                       selected = 1
+           )       
     ),
+    
+    # Main Panel for Outputs
+    column(9, 
+           mainPanel(
+             "ABV and IBU Charts",
+             fluidRow(
+               column(6, plotOutput(outputId = "dist_plot")),
+               column(6, plotOutput(outputId = "dist_plot2")),
+               column(6, plotOutput(outputId = "dist_plot3")),
+               column(12, plotOutput(outputId = "dist_plot4"))
+             )
+           )
+    ),
+  ),
+  fluidRow(
     tags$h1("GitHub Pages"),
-    tags$a(href="https://github.com/cdholmes11/cdholmes11.github.io",
-           "If you want to see more, visit my GitHub Pages")
-  ))
+    tags$a(
+      href = "https://github.com/cdholmes11/cdholmes11.github.io",
+      "If you want to see more, visit my GitHub Pages"
+    )
+  )
+)
